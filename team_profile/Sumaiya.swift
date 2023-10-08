beac69d1c675abd2b8c17b0d7e52b179c7cc128b
sumaiya-thani <sumaiyathani@gmail.com>
2023-10-08 11:37:52 +0300

    Sumaiya's profile

---
+++ team_profile/Sumaiya.swift
@@ -0,0 +1,18 @@
+//
+//  Sumaiya.swift
+//  team_profile
+//
+//  Created by sumaiya on 08/10/2566 BE.
+//
+
+import SwiftUI
+
+struct Sumaiya: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sumaiya()
+}
