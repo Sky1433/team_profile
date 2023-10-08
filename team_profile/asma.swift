a23cee737a35a342076bfc030998725bc262dc57
asmamohds <asmamohds.v@gmail.com>
2023-10-08 13:16:40 +0300

    asma1

--- team_profile/asma.swift
+++ team_profile/asma.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 struct asma: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("asma")
+                .resizable()
+                .frame(width: 250,height: 250)
+                .aspectRatio(contentMode: .fit)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white,
+                                         lineWidth: 5))
+            
+           
+            Text("ASMA").foregroundColor(.blue)
+            Text("Apple Developer Academy trainee | IOS Developer | Ui and Ux . ")
+        }
+        .fontWeight(.bold)
+        .font(.title)
+        
+        
+        
+            .padding()
     }
 }
-
 #Preview {
     asma()
 }
