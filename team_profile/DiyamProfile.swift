01c539609eff43083be9764f47e270a49ffc60e6
DiyamAlr <Deemrabah99@gmail.com>
2023-10-08 11:50:35 +0300

    Diyam's profile

---
+++ team_profile/DiyamProfile.swift
@@ -0,0 +1,18 @@
+//
+//  DiyamProfile.swift
+//  team_profile
+//
+//  Created by Diyam Alrabah on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct DiyamProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiyamProfile()
+}
