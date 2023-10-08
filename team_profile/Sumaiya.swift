2194c651b4b26bd0644fdf9474e145ea5f71cde5
sumaiya-thani <sumaiyathani@gmail.com>
2023-10-08 11:45:43 +0300

    new sumaiya

--- team_profile/Sumaiya.swift
+++ team_profile/Sumaiya.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct Sumaiya: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Image("1")
+                .resizable()
+           
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+            .aspectRatio(contentMode: .fit)
+            Text("SUMAIYA").foregroundColor(.green).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.leading)
+            Text("Game developer have a huge intrest in Vr and Ar ,Ilove working with data flow , hair style and coffee lover . ")
+                .multilineTextAlignment(.leading)
+        } .padding()
     }
 }
 
