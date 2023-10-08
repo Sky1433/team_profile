7ff3e4722af75cc43c843816af75c36cebb1d773
asmamohds <asmamohds.v@gmail.com>
2023-10-08 13:16:02 +0300

    Asma

---
+++ team_profile/asma.swift
@@ -0,0 +1,18 @@
+//
+//  asma.swift
+//  team_profile
+//
+//  Created by asma on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct asma: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    asma()
+}
