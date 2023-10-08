5c0b0bd3970bc51daa1f8e6bf09a7d2a28d34525
ppvq11 <muai0466@gmail.com>
2023-10-08 11:57:03 +0300

    Muna

---
+++ team_profile/Muna.swift
@@ -0,0 +1,18 @@
+//
+//  Muna.swift
+//  team_profile
+//
+//  Created by Muna Aiman Al-hajj on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Muna: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Muna()
+}
