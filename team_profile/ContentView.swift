5e585296ca7f75547242377769b987faf5207abf
Sky1433 <Samaanawaf@gmail.com>
2023-10-08 11:07:50 +0300

    Initial Commit

---
+++ team_profile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  team_profile
+//
+//  Created by Samaa on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ team_profile/team_profileApp.swift
@@ -0,0 +1,17 @@
+//
+//  team_profileApp.swift
+//  team_profile
+//
+//  Created by Samaa on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct team_profileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
